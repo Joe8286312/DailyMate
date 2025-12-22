ba73b6e270c126a460050ce6659fb565317c4946
Joe <joe8286312@foxmail.com>
2025-12-22 10:44:14 +0800

    初始化数据库和后端

---
+++ backend/dailymate/src/test/java/com/joe/dailymate/DailymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joe.dailymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
