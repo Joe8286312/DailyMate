6934edc879f6377da1695195222ea3270ae147eb
谢苏阳 <1823467363@qq.com>
2025-12-27 17:10:07 +0800

    前端页面完成，还未完成前后端联调

--- backend/dailymate/src/main/java/com/joe/dailymate/config/SecurityConfig.java
+++ backend/dailymate/src/main/java/com/joe/dailymate/config/SecurityConfig.java
@@ -1,4 +1,3 @@
-// SecurityConfig.java
 package com.joe.dailymate.config;
 
 import com.joe.dailymate.filter.JwtAuthenticationFilter;
@@ -7,15 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                       .requestMatchers("/api/auth/**").permitAll() // 放行注册和登录接口
-                        // .requestMatchers("/api/**").permitAll() // 放行全部后端接口，调试用
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // 放行所有OPTIONS预检请求
+                        .requestMatchers("/api/auth/**").permitAll() // 放行注册和登录接口
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
---
+++ frontend/dailymate/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!-- <h1>{{message}}</h1>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row> -->
+    <dailymate-view></dailymate-view>
+  </div>
+</template>
+
+<script>
+import DailymateView from './views/elment/DailymateView.vue'
+
+
+    export default{
+      components:{ DailymateView },
+      data(){
+        return{
+          message:"hello vue!"
+        }
+      },
+      methods:{
+            
+      }
+    }
+</script>
+
+<style>
+    h1{
+        color:red;
+    }
+</style>
